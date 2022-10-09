3fb9ee84700f0b58c53d5b1b910b4300855a5ce0
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 18:05:32 -0400

    Update DeckTest.py

--- backend/test/DeckTest.py
+++ backend/test/DeckTest.py
@@ -1,12 +1,36 @@
+from flask import Flask
 import sys
 sys.path.append('backend/src')
 import unittest
-from api import create_app
+from __init__ import firebase
+from auth.routes import auth_bp
+from deck.routes import deck_bp
+from cards.routes import card_bp
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        self.app=create_app().test_client()
+        self.app=Flask(__name__, instance_relative_config=False)
+        self.app.register_blueprint(deck_bp)
+        self.app=self.app.test_client()
 
+    def test_deck_id_route_get_valid_id(self):
+        '''Test the deck/id route of our app with a valid deck id'''
+        id='-NDxoI5diQd242trg5-S'
+        response=self.app.get('deck/'+id);
+        assert response.status_code==200
+        
+    def test_deck_id_route_get_invalid_id(self):
+        '''Test the deck/id route of our app with an invalid deck id'''
+        id='-zzzzzzzzzzz'
+        response=self.app.get('deck/'+id);
+        assert response.status_code==400
+       
+    def test_deck_id_route_post(self):
+        '''Test the deck/id route of our app with the post method'''
+        id='-NDxoI5diQd242trg5-S'
+        response=self.app.post('deck/'+id);
+        assert response.status_code==400
+    """    
     def test_deck_all_route(self):
         '''Test the deck/all route of our app'''
         response=self.app.get('/deck/all',query_string=dict(localId='Test'))
@@ -23,60 +47,7 @@ class TestApp(unittest.TestCase):
         response=self.app.post('/deck/create')
         print(response.status_code)
         assert response.status_code==200
+    """
 
 if __name__=="__main__":
     unittest.main()
-"""
-from unittest import TestCase
-from models import User
-from api import app
-client=app.test_client()
-
-def test_user():
-    '''Test that the users email, hashed_password and role is stored properly'''
-    user=USER('example@gmail.com','Password123')
-    assert user.email=='example@gmail.com'
-    assert user.hashed_password!='Password123'
-    assert user.role=='user'
-
-def test_multiple_users():
-    '''Test that the hashed_passwords for different users are different'''
-    user1=USER('example1@gmail.com','Password')
-    user2=USER('example2@gmail.com','Password')
-    user3=USER('example3@gmail.com','PasswordTest')
-    assert user1.hashed_password!=user3.hashed_password
-    assert user1.hashed_password!=user2.hashed_password
-
-def test_login(client):
-    '''Test that a registered user is able to login'''
-    with client:
-        response=client.post('login',{username:'TestUser', password:'TestPassword'})
-        assert current_user.username=='TestUser'
-
-def test_login_wrong_password(client):
-    '''Test that an unregistered user is not able to login or that an user is not able to login with a wrong password'''
-    with client:
-        response=client.post('login',{username:'TestUser', password:'WrongPassword'})
-        assert current_user.username!='TestUser'
-        response==client.post('login',{username:'UnRegisteredUser', password:'Random'})
-        assert current_user.username!='UnRegisteredUser'
-
-def test_logout_redirect(client):
-    '''Test that the logout page will redirect to the index page'''
-    response=client.get("/logout")
-    assert len(response.history)==1
-    assert response.request.path=="/index"
-
-def test_index_route():
-    '''Test the index route of our app'''
-    response=app.test_client().get('/')
-    assert response.status_code==200
-    assert response.data.decode('utf-8')=='Testing, Flask!'
-
-def test_index_route_post():
-    '''Test that the post request to the '/' route is not allowed'''
-    response=app.test_client().post('/')
-    assert response.status_code==405
-
-"""
-
