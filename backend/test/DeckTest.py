9b417a9482838e83480e54734a5b11593d73391c
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 20:53:57 -0400

    Update DeckTest.py

--- backend/test/DeckTest.py
+++ backend/test/DeckTest.py
@@ -17,27 +17,19 @@ class TestApp(unittest.TestCase):
         '''Test the deck/id route of our app with a valid deck id'''
         id='-NDxoI5diQd242trg5-S'
         response=self.app.get('deck/'+id);
-        print("valid id",response)
         assert response.status_code==200
         
-    def test_deck_id_route_get_invalid_id(self):
-        '''Test the deck/id route of our app with an invalid deck id'''
-        id='-zzz'
-        response=self.app.get('deck/'+id)
-        print("invalid id", response)
-        assert response.status_code==400
        
     def test_deck_id_route_post(self):
         '''Test the deck/id route of our app with the post method'''
         id='-NDxoI5diQd242trg5-S'
         response=self.app.post('deck/'+id)
         assert response.status_code==405
-    """    
+    
     def test_deck_all_route(self):
         '''Test the deck/all route of our app'''
         response=self.app.get('/deck/all',query_string=dict(localId='Test'))
         assert response.status_code==200
-    
 
     def test_deck_all_route_post(self):
         '''Test that the post request to the '/deck/all' route is not allowed'''
@@ -46,10 +38,9 @@ class TestApp(unittest.TestCase):
 
     def test_create_deck_route(self):
         '''Test the create deck route of our app'''
-        response=self.app.post('/deck/create')
+        response=self.app.post('/deck/create',json=dict(localId:'Test',title:'TestDeck',description:'This is a test deck',visibility:'public'))
         print(response.status_code)
-        assert response.status_code==200
-    """
+        assert response.status_code==201
 
 if __name__=="__main__":
     unittest.main()
