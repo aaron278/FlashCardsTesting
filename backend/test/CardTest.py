852ef52717387f1223356ca14996d1eb48b9675e
aaron278 <112004832+aaron278@users.noreply.github.com>
2022-10-09 23:39:29 -0400

    Update CardTest.py

--- backend/test/CardTest.py
+++ backend/test/CardTest.py
@@ -46,7 +46,7 @@ class CardTestApp(unittest.TestCase):
             self.app.post('/login',json=dict(email='aaronadb@gmail.com',password='flashcards123'),follow_redirects=True)
             self.app.post('/deck/create',json=dict(localId='Test',title='TestDeck',description='This is a test deck',visibility='public'))
             self.app.post('/deck/Test/card/create',json=dict(localId='Test',cards=[dict(front='front',back='back',hint='hint')]))        
-            response=self.app.patch('/deck/Test/update/TestCard',json=dict(word='word',meaning='meaning'))
+            response=self.app.patch('/deck/Test/update/Test',json=dict(word='word',meaning='meaning'))
             print('update',response.status_code)
             assert response.status_code==201
      
@@ -56,7 +56,7 @@ class CardTestApp(unittest.TestCase):
             self.app.post('/login',json=dict(email='aaronadb@gmail.com',password='flashcards123'),follow_redirects=True)
             self.app.post('/deck/create',json=dict(localId='Test',title='TestDeck',description='This is a test deck',visibility='public'))
             self.app.post('/deck/Test/card/create',json=dict(localId='Test',cards=[dict(front='front',back='back',hint='hint')]))        
-            response=self.app.delete('/deck/Test/delete/TestCard')
+            response=self.app.delete('/deck/Test/delete/Test')
             print('delete',response.status_code)
             assert response.status_code==200
 
